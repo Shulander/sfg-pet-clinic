7ada143b248b12bfb290dfc9bf32f7f7285e7ab6
Henrique Vicentini <henrique@vicentini.us>
2019-09-21 12:05:47 -0700

    class 3.54. Spring Pet Clinic - POJO Data Model
    add name property to PetType

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,4 +1,11 @@
 package guru.springframework.sfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PetType extends BaseEntity {
+
+    private String name;
 }
