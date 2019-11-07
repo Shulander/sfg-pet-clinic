a1e57b61d072d8812f3819913e546001eb9f5d39
Henrique Vicentini <henrique@vicentini.us>
2019-11-06 21:18:32 -0800

    class 7.123 Apply Master Layout to Vet Page

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.services.VetService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class VetController {
 
@@ -14,8 +16,9 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets.html", "/vets/index.html"})
     public String listVets(Model model) {
+        log.info("vets mapping");
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
