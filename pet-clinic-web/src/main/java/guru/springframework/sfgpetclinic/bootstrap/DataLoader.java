e5e3eb81c37afb20c19b27e8e20f837c1465c569
Henrique Vicentini <henrique@vicentini.us>
2019-11-06 21:46:37 -0800

    class 7.129 PetType data on startup

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,22 +1,35 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Override
     public void run(String... args) throws Exception {
+        log.info("Data Loader started");
+        PetType dogType = new PetType();
+        dogType.setName("Dog");
+        PetType savedDogType = petTypeService.save(dogType);
+        PetType catType = new PetType();
+        catType.setName("Dog");
+        PetType savedCatType = petTypeService.save(catType);
+        log.info("Loaded PetType...");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -28,7 +41,7 @@ public class DataLoader implements CommandLineRunner {
         owner2.setLastName("Glenanne");
         ownerService.save(owner2);
 
-        System.out.println("Loaded Owners...");
+        log.info("Loaded Owners...");
 
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
@@ -40,6 +53,6 @@ public class DataLoader implements CommandLineRunner {
         vet2.setLastName("Porter");
         vetService.save(vet2);
 
-        System.out.println("Loaded Vets...");
+        log.info("Loaded Vets...");
     }
 }
