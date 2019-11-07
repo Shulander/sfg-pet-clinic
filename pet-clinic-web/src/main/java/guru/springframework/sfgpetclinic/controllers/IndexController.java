928d2ad9e7934609734819eb511d5f27d6b54c8c
Henrique Vicentini <henrique@vicentini.us>
2019-11-06 21:09:53 -0800

    class 7.121 Internationalization Properties

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,13 +1,16 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "index.html"})
     public String index() {
+        log.info("index mapping");
         return "index";
     }
 }
